40a6dab6c424f3916b3e8addf8d98d0b6ef51a1c
papa-react <makiya112545@gmail.com>
2022-01-25 18:09:45 -1200

    typeing State

--- src/type.ts
+++ src/type.ts
@@ -1,1 +1,9 @@
 export type nameType = { name: string };
+export type IState = { 
+    people: { 
+        name: string, 
+        age: number, 
+        url: string
+        note?: string
+    }[]|null
+}
