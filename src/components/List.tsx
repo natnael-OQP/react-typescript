b20702e522bbcb1ec5f5b57cf9587b00e1c3d5a9
papa-react <makiya112545@gmail.com>
2022-01-25 18:21:51 -1200

    style paragraphe

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -6,17 +6,21 @@ const List: React.FC<IState> = ({ people }) => {
 		return (
 			<>
 				{people?.map((person) => (
-					<li className="my-2 shadow-md rounded-md bg-gray-100 p-2 border border-gray-900 flex items-center justify-between max-w-3xl mx-auto text-gray-600 font-semibold ">
-						<div className="flex items-center space-x-3">
+					<li className=" cursor-pointer my-2 shadow-md rounded-md bg-gray-100 p-2 border border-gray-900 flex items-center justify-between max-w-3xl mx-auto text-gray-600 font-semibold ">
+						<div className="flex items-center space-x-3 flex-[20] shrink-0">
 							<img
-								className="w-14 h-14 rounded-full"
+								className="w-14 h-14 rounded-full shrink-0"
 								src={person.url}
 								alt={person.name}
 							/>
-							<h5>{person.name}</h5>
+							<h5 className="text-lg font-bold text-slate-600">
+								{person.name}
+							</h5>
 						</div>
-						<h4>{person.age} Years Old</h4>
-						<p>{person.note}</p>
+						<h4 className="w-36 shrink-0">
+							{person.age} Years Old
+						</h4>
+						<p className="w-[65%] text-left">{person.note}</p>
 					</li>
 				))}
 			</>
